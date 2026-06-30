290d0b0ff0c24061944c50ef512ff5706fd543b5
Felipe Satoru Meucci <2025311470@aluno.riogrande.ifrs.edu.br>
2026-06-29 21:57:06 -0300

    script.js
    Initial commit of script.js with comments outlining features and functionalities.

---
+++ Trabalho2front/JS/script.js
@@ -0,0 +1,39 @@
+//imagens como o da mostrado no pdf do trabalho mas com uma imagem inteira que esta recortada 
+
+//imagem, sopa de letras, e as tetras do questionario saem de dentro da imagem da sopa de letras //impossivel sem js
+//formulario sobre sopa preferida
+/*
+Nome
+Seu nome completo
+-Email
+seu@email.com
+Sopa favorita
+
+Escolha um tipo
+Última vez que cozinhou
+
+dd/mm/aaaa
+Dificuldade
+Fácil --------<->--- Difícil
+Comentário
+-------------------------------------
+Conte algo sobre sua sopa favorita...
+-------------------------------------
+_
+|| Sim, quero receber noticias sobre novas receitas
+_ */
+
+
+
+//fazer receitas com "barra de rolagem para lateral, sem ser vertical"
+
+//fazer elementos dos tipos de receitar aparecerem conforme rolagem da tela
+        //fazer rolagem apenas do conteudo presente em tipos de sopa ao inves do site interio
+
+
+
+//na parte inicial do site fazer com que a imagem fique fixa e o site todo rola para baixo ao inves da imagem principal
+
+
+
+//tem banner animadooooo!!!!!!!!!
